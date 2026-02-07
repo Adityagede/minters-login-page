fbe5a9261bf1d37325b65b7e506e6bfd4f566c45
Gede-aditya <gedeaditya110@gmail.com>
2026-02-07 09:40:19 +0800

    menambahkan semua file

---
+++ final.js
@@ -0,0 +1,32 @@
+// js dark light mode
+const themeBtn = document.getElementById("themeBtn");
+const body = document.body;
+
+
+themeBtn.addEventListener("click", () => {
+  if (body.classList.contains("dark")) {
+    body.classList.replace("dark", "light");
+    themeBtn.textContent = "☀ Light";
+  } else {
+    body.classList.replace("light", "dark");
+    themeBtn.textContent = "🌙 Dark";
+  }
+});
+
+const loginbtn = document.getElementById("btn-id")
+
+const form = document.querySelector("form");
+console.log(form);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email == "contoh@gmail.com" && password == "11111"){
+    alert ("Login Successful");
+    window.location.href ="dashboard.html"
+  } else {
+    alert ("Invalid email or password");
+  }
+})
