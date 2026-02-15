bfc234cc73320b0bd9111758ea54d5963148893f
Gede-aditya <gedeaditya110@gmail.com>
2026-02-15 13:19:45 +0800

    tambah kata-kata

--- final.js
+++ final.js
@@ -23,7 +23,7 @@ form.addEventListener("submit", (e) => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  if (email == "contoh@gmail.com" && password == "11111"){
+  if (email == "budi@gmail.com" && password == "1234"){
     alert ("Login Successful");
     window.location.href ="dashboard.html"
   } else {
